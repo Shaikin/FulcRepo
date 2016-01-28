af407d3df03d40b12ffcd2a40e9df2a411e8990d
Shaik <sha_ikin@yahoo.co.in>
2016-01-28 12:17:50 +0530

    cleared unused imports

--- Falcrum_Trunk/src/com/proj/library/commonMethods.java
+++ Falcrum_Trunk/src/com/proj/library/commonMethods.java
@@ -11,7 +11,6 @@ import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.RemoteWebDriver;
-import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.ITestResult;
